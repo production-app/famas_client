aba7d5b0a2cd2e6c566f4725c261aa643a7455f0
Mona Lisa <email@example.com>
2024-02-02 01:06:05 +0100

    updating the code

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -23,6 +23,37 @@ const page: React.FC = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const [value, setValue] = useState("");
   const [modalValue, setModalValue] = useState("");
+  const [modalID, setModalID] = useState("");
+  const [modalState, setModalState] = useState(false);
+
+  const { data, isLoading, refetch } = useQuery(["fetch"], async () => {
+    originData = [];
+    try {
+      const res = await fetch(`http://localhost:5600/v1/location`);
+      const res_1 = await res.json();
+      //setLoading(true);
+      originData = [];
+      res_1.data.forEach(({ id, name }: any, index: any) => {
+        // console.log(name);
+        originData.push({
+          locationName: name,
+          id: id,
+        });
+      });
+      //setDataSource(originData);
+      return originData;
+    } finally {
+    }
+  });
+
+  const confirmDelete = async (id: any) => {
+    console.log(id);
+    await axios.delete(`http://localhost:5600/v1/location/${id}`).then(() => {
+      // getAllLocation();
+      refetch();
+      message.success("Item Deleted !");
+    });
+  };
 
   const columns = [
     {
@@ -53,64 +84,37 @@ const page: React.FC = () => {
                 component="button"
                 color="lime"
                 onClick={async () => {
-                  fetch(
-                    `http://localhost:9001/v1/get-single-location/${record.uuid}`
-                  )
+                  fetch(`http://localhost:5600/v1/location/${record.id}`)
                     .then((res) => res.json())
                     .then((res: any) => {
-                      const { uuid, locationName } = res.data;
-                      // setModalValue(locationName);
-                      //  setModalID(uuid);
-                      //  setModalState(true);
+                      console.log(res.data);
+                      const { id, name } = res.data;
+                      setModalValue(name);
+                      setModalID(id);
+                      setModalState(true);
                     });
                   open();
                 }}
               >
                 <IconEdit />
               </ActionIcon>
-              <ActionIcon
-                variant="subtle"
-                component="button"
-                color="red"
-                onClick={async () => {
-                  //   axios
-                  //     .delete(
-                  //       `http://localhost:9001/v1/delete-location/${record.uuid}`
-                  //     )
-                  //     .then(() => {
-                  //       getAllLocation();
-                  //       message.success("Item Deleted !");
-                  //     });
-                }}
+              <Popconfirm
+                title="Would you like to delete?"
+                description="The operation will be completed once clicked."
+                onConfirm={() => confirmDelete(record.id)}
+                okText="Yes"
+                cancelText="No"
               >
-                <IconTrash />
-              </ActionIcon>
+                <ActionIcon variant="subtle" component="button" color="red">
+                  <IconTrash />
+                </ActionIcon>
+              </Popconfirm>
             </Group>
           </>
         );
       },
     },
   ];
-
-  const { data, isLoading, refetch } = useQuery(["fetch"], async () => {
-    originData = [];
-    try {
-      const res = await fetch(`http://localhost:5600/v1/location`);
-      const res_1 = await res.json();
-      //setLoading(true);
-      originData = [];
-      res_1.data.forEach(({ id, name }: any, index: any) => {
-        // console.log(name);
-        originData.push({
-          locationName: name,
-          id: id,
-        });
-      });
-      //setDataSource(originData);
-      return originData;
-    } finally {
-    }
-  });
 
   const confirmAdd = async (e: any) => {
     // message.success("Click on Yes");
@@ -126,29 +130,73 @@ const page: React.FC = () => {
       });
   };
 
+  const confirmPut = async () => {
+    await axios
+      .put(`http://localhost:5600/v1/location/${modalID}`, {
+        name: modalValue,
+      })
+      .then(async () => {
+        message.success("Successfully Updated!");
+        close(),
+          //getAllLocation(),
+          setModalValue("");
+        ///update-location/
+
+        refetch();
+        setModalState(false);
+      });
+  };
+
   return (
     <>
       <Modal opened={opened} onClose={close}>
         <Divider style={{ paddingTop: 20 }} />
         <Group style={{ padding: 5 }}>
-          <TextInput
-            label="Add Location"
-            placeholder="Location"
-            radius={5}
-            value={value || modalValue}
-            onChange={(event) => setValue(event.currentTarget.value)}
-          />
-          <Popconfirm
-            title="Would you like to submit?"
-            description="The operation will be completed once confirmed."
-            // onConfirm={confirm}
-            onConfirm={confirmAdd}
-            //onCancel={null}
-            okText="Yes"
-            cancelText="No"
-          >
-            <Button style={{ marginBottom: -23 }}>Submit</Button>
-          </Popconfirm>
+          {!modalState ? (
+            <TextInput
+              label="Add Location"
+              placeholder="Location"
+              radius={5}
+              value={value || modalValue}
+              onChange={(event) => setValue(event.currentTarget.value)}
+            />
+          ) : (
+            <TextInput
+              label="Add Location"
+              placeholder="Location"
+              radius={5}
+              value={modalValue}
+              onChange={(event) => setModalValue(event.currentTarget.value)}
+            />
+          )}
+
+          {!modalValue ? (
+            <Popconfirm
+              title="Would you like to submit?"
+              description="The operation will be completed once confirmed."
+              // onConfirm={confirm}
+              onConfirm={confirmAdd}
+              //onCancel={null}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button style={{ marginBottom: -23 }}>Submit</Button>
+            </Popconfirm>
+          ) : (
+            <Popconfirm
+              title="Would you like to update?"
+              description="The operation will be completed once confirmed."
+              // onConfirm={confirm}
+              onConfirm={confirmPut}
+              //onCancel={null}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button style={{ marginBottom: -23 }} color="lime">
+                Update
+              </Button>
+            </Popconfirm>
+          )}
         </Group>
       </Modal>
       <DashboardLayout>
